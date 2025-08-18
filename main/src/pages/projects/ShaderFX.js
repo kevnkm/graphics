67f81c70d7507d967681b04e1c440f6b1b9b8736
Kevin Kim <kk946@cornell.edu>
2025-08-18 15:05:34 +0900

    Create ShaderFX.js

---
+++ main/src/pages/projects/ShaderFX.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { useState } from "react";
+import * as ShaderFXCanvas from "../../components/ShaderFXCanvas";
+
+const componentGroupList = [
+  {
+    groupName: "2D Voronoi Diagram",
+    description: (
+      <>
+        This interactive scene visualizes a 2D Voronoi diagram using a simple
+        algorithm based on Euclidean distance. The diagram is created by
+        partitioning the space into regions around a set of seed points. Each
+        region corresponds to the points closest to a particular seed, forming
+        the characteristic polygonal shapes.
+      </>
+    ),
+    components: {
+      A1: () => <ShaderFXCanvas.A1 />,
+    },
+  },
+  {
+    groupName: "3D Ray Marching",
+    description: (
+      <>
+        This scene showcases a 3D ray marching renderer that visualizes complex
+        shapes like spheres, toruses, and cubes using Signed Distance Fields
+        (SDF). A reference for various SDFs including the ones used in this
+        project can be found at{" "}
+        <a
+          href="https://iquilezles.org/articles/distfunctions/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline ml-1"
+        >
+          this page by Inigo Quilez
+        </a>
+        . Instead of traditional rendering methods, the algorithm marches rays
+        step by step through space, dynamically adjusting their movement based
+        on the closest object's distance. A key feature of this approach is
+        smooth blending, allowing nearby shapes to merge seamlessly.
+      </>
+    ),
+    components: {
+      B1: () => <ShaderFXCanvas.B1 />,
+    },
+  },
+];
+
+function ShaderFX() {
+  const [activeTabs, setActiveTabs] = useState({
+    "2D Voronoi Diagram": "A1",
+    "3D Ray Marching": "B1",
+  });
+
+  const handleTabChange = (groupName, tab) => {
+    setActiveTabs((prev) => ({
+      ...prev,
+      [groupName]: tab,
+    }));
+  };
+
+  return (
+    <div className="relative w-full min-h-[calc(100vh)] bg-white flex justify-center">
+      <div className="container mx-auto p-4 w-full max-w-md sm:max-w-2xl lg:max-w-3xl pb-16">
+        <h1 className="text-xl font-bold text-center text-gray-800 mb-3">
+          Introduction
+        </h1>
+        <p className="text-center">
+          I'm personally not a good designer. However, it seems like math is
+          something I can do. This project is a collection of some math-based
+          visualizations that I have created. The goal is to explore the
+          intersection of math and design, and to create visually appealing
+          graphics that are also mathematically interesting.
+        </p>
+
+        {componentGroupList.map((group, index) => (
+          <React.Fragment key={group.groupName}>
+            <br />
+            <h1 className="text-xl font-bold text-center text-gray-800 mb-3">
+              {group.groupName}
+            </h1>
+            <p className="text-center">{group.description}</p>
+
+            <div className="flex flex-wrap justify-center items-center gap-5 w-full mt-5">
+              {group.components[activeTabs[group.groupName]]()}
+            </div>
+
+            <div className="p-3 bg-gray-200 rounded-bl-lg rounded-br-lg">
+              <div className="flex overflow-x-auto gap-3 pb-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+                {Object.keys(group.components).map((key) => (
+                  <button
+                    key={key}
+                    onClick={() => handleTabChange(group.groupName, key)}
+                    className={`px-4 py-2 rounded-lg whitespace-nowrap ${
+                      activeTabs[group.groupName] === key
+                        ? "bg-blue-500 text-white"
+                        : "bg-white text-black"
+                    }`}
+                  >
+                    {key}
+                  </button>
+                ))}
+              </div>
+            </div>
+            {index < componentGroupList.length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ShaderFX;
